38957c5632ba698cb8bf1c36e3536af8740223c2
Dongasai <1514582970@qq.com>
2018-04-03 17:52:15 +0800

    修改bug

--- index.php
+++ index.php
@@ -3,17 +3,18 @@
 
 # 设置php常用配置
 date_default_timezone_set("PRC");
-
+# 加载函数库
+include __DIR__.'./function.php';
 # 设置 常量
 defined('ROOT_DIR') || exit('constant ROOT_DIR Undefined!');
 define('PMS_DIR', __DIR__);
+
 echo '项目目录为:' . ROOT_DIR  .',pms目录为:' . PMS_DIR . " \n";
 defined('RUNTIME_DIR') || define('RUNTIME_DIR',ROOT_DIR. './runtime/');# 运行目录
 defined('CACHE_DIR') || define('CACHE_DIR', ROOT_DIR.'./runtime/cache/');# 缓存目录
 defined('CACHE_DIR') || define('APP_DEBUG', boolval(get_env("APP_DEBUG", 1)));# debug 的开启
 defined('CACHE_DIR') || define('PACKAGE_EOF', '_pms_');
-# 加载函数库
-include PMS_DIR.'./function.php';
+
 # 服务的地址和端口
 if (empty(get_env("APP_HOST_IP"))) {
     $ip_list = swoole_get_local_ip();

0fd9f33da2c40f991d43a6e601eb7917c46ea1fc
Dongasai <1514582970@qq.com>
2018-04-16 16:12:23 +0800

    修正一些BUG

---
+++ Validation/Message/Group.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace pms\Validation\Message;
+
+
+/**
+ * Class Group
+ * @package pms\Validation\Message
+ */
+class Group extends \Phalcon\Validation\Message\Group
+{
+    /**
+     * 转换数组
+     * @return array
+     */
+    public function toArray()
+    {
+        $message_string = '';
+        $arr = [
+            'message' => '',
+            'data' => []
+        ];
+        foreach ($this->_messages as $key => $message) {
+            if ($message instanceof \Phalcon\Validation\Message) {
+                $index = $message->getMessage() . '-' . $message->getType() . '-' . $message->getField();
+                if ($key) {
+                    $message_string .= ' & ';
+                }
+                $message_string .= $index;
+                $arr['data'][] = [
+                    'text' => $message->getMessage(),
+                    'code' => $message->getCode(),
+                    'field' => $message->getField(),
+                    'type' => $message->getType()
+                ];
+            }
+        }
+        $arr['message'] = $message_string;
+        return $arr;
+    }
+
+}

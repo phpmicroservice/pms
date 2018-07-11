2d025cb386a578db8c2b6b41a6edde7db89e83a2
Dongasai <1514582970@qq.com>
2018-07-11 15:28:16 +0800

    修改一些常量的设置,runtime_dir,cache_dir,log_dir目录的自动创建

--- src/index.php
+++ src/index.php
@@ -30,7 +30,11 @@ if (defined('SERVICE_NAME')) {
 
 
 defined('RUNTIME_DIR') || define('RUNTIME_DIR', ROOT_DIR . '/runtime/');# 运行目录
-defined('CACHE_DIR') || define('CACHE_DIR', ROOT_DIR . '/runtime/cache/');# 缓存目录
+if (!is_dir(RUNTIME_DIR)) mkdir(RUNTIME_DIR, 775, true);
+defined('CACHE_DIR') || define('CACHE_DIR', RUNTIME_DIR . '/cache/');# 缓存目录
+if (!is_dir(CACHE_DIR)) mkdir(CACHE_DIR, 775, true);
+defined('LOG_DIR') || define('LOG_DIR', RUNTIME_DIR . 'log/');# 日志目录
+if (!is_dir(LOG_DIR)) mkdir(LOG_DIR, 775, true);
 defined('APP_DEBUG') || define('APP_DEBUG', boolval(get_env("APP_DEBUG", 1)));# debug 的开启
 # 输出级别定义
 defined('OUTPUT_ERROR') || define('OUTPUT_ERROR', get_envbl("OUTPUT_ERROR", 1));# error级别的输出 的开启
